ade7819c012875c12e35bba2147cd3e58c6c6d8e
burgosha <hernan.a.burgos@gmail.com>
2025-09-11 21:27:44 -0300

    Cliente

--- models/ClienteModel.js
+++ models/ClienteModel.js
@@ -1,15 +1,58 @@
-// models/Cliente.js
+// models/ClienteModel.js
+const database = require("../data/database");
 
-class Cliente {
-  constructor(idCliente, nombre, apellido, razonSocial, direccion, telefono, mail) {
-    this.idCliente = idCliente;
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.razonSocial = razonSocial;
-    this.direccion = direccion;
-    this.telefono = telefono;
-    this.mail = mail;
-  }
+class ClienteModel {
+    // Obtener todos
+    getClientes() {
+        return database.clientes;
+    }
+
+    // Obtener por ID
+    getById(id) {
+        return database.clientes.find(c => c.id === parseInt(id)) || null;
+    }
+
+    // Agregar nuevo
+    addCliente(clienteData) {
+        const nuevoCliente = {
+            id: database.clientes.length + 1,
+            nombre: clienteData.nombre,
+            apellido: clienteData.apellido,
+            razonSocial: clienteData.razonSocial,
+            direccion: clienteData.direccion || "",
+            telefono: clienteData.telefono || "",
+            mail: clienteData.mail || ""
+        };
+        database.clientes.push(nuevoCliente);
+        return nuevoCliente;
+    }
+
+    // Actualizar completo
+    updateCliente(id, clienteData) {
+        const index = database.clientes.findIndex(c => c.id === parseInt(id));
+        if (index === -1) return null;
+
+        database.clientes[index] = { ...database.clientes[index], ...clienteData };
+        return database.clientes[index];
+    }
+
+    // Actualizar parcial
+    patchCliente(id, campos) {
+        return this.updateCliente(id, campos);
+    }
+
+    // Eliminar
+    removeCliente(id) {
+        // Validar si tiene envíos antes de eliminar
+        if (database.clienteTieneEnvios(id)) {
+            throw new Error("No se puede eliminar el cliente porque tiene envíos asociados");
+        }
+
+        const index = database.clientes.findIndex(c => c.id === parseInt(id));
+        if (index === -1) return null;
+
+        return database.clientes.splice(index, 1)[0];
+    }
 }
 
-module.exports = Cliente;
+module.exports = new ClienteModel();
