0c6a4f4b19c30db48b9e90f488732d861f0b7ed4
nicochiove <nicochiove@gmail.com>
2025-10-15 19:52:19 -0300

    Agrego model en mongo para Vehiculo y actualizo controller y el dashboard

--- controllers/VehiculoController.js
+++ controllers/VehiculoController.js
@@ -1,12 +1,22 @@
 const VehiculoModel = require("../models/VehiculoModel");
+var mongoose = require('mongoose');
+var Vehiculo = mongoose.model('VehiculoModel');
 
-const getVehiculos = (req, res) => {
-    try {
-        res.json(VehiculoModel.getVehiculos());
-    } catch (error) {
-        res.status(500).json({ message: "Error al obtener Vehiculos", error: error.message });
+const getVehiculos = async (req, res) =>{
+    try{
+        const vehiculos = await Vehiculo.find({});
+        res.status(200).json(vehiculos);
+    }catch(err){
+        res.status(500).json({ error: err.message });
     }
 };
+// const getVehiculos = (req, res) => {
+//     try {
+//         res.json(VehiculoModel.getVehiculos());
+//     } catch (error) {
+//         res.status(500).json({ message: "Error al obtener Vehiculos", error: error.message });
+//     }
+// };
 
 module.exports = {
     getVehiculos
